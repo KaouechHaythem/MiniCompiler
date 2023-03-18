7cb3a8b8d8a47eba14b92dc0eb92c4850f95fe79
haythemkaouech <haythemkaouech@gmail.com>
2023-03-18 14:09:29 +0100

    cleaning

--- minicompilerfront/src/app/app.module.ts
+++ minicompilerfront/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SpinnerComponent } from './spinner/spinner.component';
 import { ContentComponent } from './content/content.component';
-import { InputCardComponent } from './input-card/input-card.component';
-import { OutputCardComponent } from './output-card/output-card.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -16,10 +13,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    SpinnerComponent,
+
     ContentComponent,
-    InputCardComponent,
-    OutputCardComponent
+   
   ],
   imports: [
     BrowserModule,
